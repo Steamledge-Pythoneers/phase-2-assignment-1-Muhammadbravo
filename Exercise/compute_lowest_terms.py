985fab2e26d58bca370969db5603bccb2d9964d8
Muhd Bravo <Muhammadelbravo@gmail.com>
2020-10-02 18:36:52 +0100

    lowest term function was completed

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,4 +1,12 @@
 ## TODO: complete the function "lowest_terms" below
 
+def fraction(x, y):
+    for i in range(2, y+1):
+        if (x % i == 0) and (y % i == 0):
+            a = int(x / i)
+            b = int(y / i)
+    return a, b
 def lowest_terms(x):
-	return ""
+	sp = x.split("/")
+	y = fraction(int(sp[0]), int(sp[1]))
+	return str(y[0]) +"/"+str(y[1])
